c52e6b7462ed31bee0569bdaf933fd15834585af
Andres Vargas <andrewgonzlez@gmail.com>
2024-09-03 13:06:31 -0500

    deploy

--- index.js
+++ index.js
@@ -2,24 +2,37 @@ const express = require('express');
 const routerApi = require('./routes');
 const cors = require('cors');
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const {
+  logErrors,
+  errorHandler,
+  boomErrorHandler,
+} = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('No permitido'));
+    }
+  },
+};
+
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola, mi primer servidor con express');
 });
 
-
 routerApi(app);
 app.use(logErrors);
 app.use(boomErrorHandler);
 app.use(errorHandler);
-
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
